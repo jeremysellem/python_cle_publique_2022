a7718862f95be9afd7f2949364c078d8f89aa8cf
Jeremy Sellem <jeremsellem@gmail.com>
2022-05-15 21:27:50 +0000

    Ajout des modules

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,3 +1,3 @@
 git submodule add -b stable https://github.com/pybind/pybind11 pybind11
 git submodule add -b master https://github.com/jluuM2/micro-ecc composant_cle/micro-ecc
-git submodule add -b main https://github.com/jeremysellem/Conversion_String_binaire.git composant_cle/conv-string-bin
+git submodule add -b main https://github.com/jluuM2/Conversion_String_binaire.git composant_cle/conv-string-bin
